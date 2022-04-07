b38795090e130f359cb3693aeabe3c21afc71c85
Kasper <49065448+Kine7ics@users.noreply.github.com>
2022-04-07 18:07:22 +0200

    Fixes bug in the MvcInstaller
    program would not run due to it missing the controllers service

--- Recommendating.Api/Installers/MvcInstaller.cs
+++ Recommendating.Api/Installers/MvcInstaller.cs
@@ -6,6 +6,8 @@ public class MvcInstaller : IInstaller
 {
     public void InstallServices(IServiceCollection services, IConfiguration configuration)
     {
+        services.AddControllers();
+        
         services.AddSwaggerGen(c =>
         {
             c.SwaggerDoc("v1", new OpenApiInfo {Title = "Recommendating", Version = "v1"});
