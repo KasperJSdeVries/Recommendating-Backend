f5f7f5151081776927b3bb945db19bf8ee524543
Kasper <49065448+Kine7ics@users.noreply.github.com>
2022-04-07 20:13:27 +0200

    Adds a message entity

---
+++ Recommendating.Api/Entities/Message.cs
@@ -0,0 +1,18 @@
+﻿namespace Recommendating.Api.Entities;
+
+public class Message
+{
+    public enum MessageStatus
+    {
+        Sent,
+        Delivered,
+        Read
+    }
+
+    public Guid Id { get; set; }
+    public User Sender { get; set; }
+    public User Receiver { get; set; }
+    public string Text { get; set; }
+    public MessageStatus Status { get; set; }
+    public DateTimeOffset SentDate { get; set; }
+}
