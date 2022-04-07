f425e4daedde5db7e5e95bc75fa4c603658b9d20
Kasper <49065448+Kine7ics@users.noreply.github.com>
2022-04-07 18:11:54 +0200

    Removes unnecessary initialization from the User Class

--- Recommendating.Api/Entities/User.cs
+++ Recommendating.Api/Entities/User.cs
@@ -5,9 +5,7 @@ namespace Recommendating.Api.Entities;
 public class User
 {
     public Guid Id { get; set; }
-
-    [NotNull] public string Name { get; set; }
-
-    public string Password { get; set; } = string.Empty;
+    public string Name { get; set; }
+    public string Password { get; set; }
     public DateTimeOffset CreatedDate { get; set; }
 }
